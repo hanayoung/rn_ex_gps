e90e7fb88432eec870cdab8357d21abd07f7e760
hay0107 <hay0107@ajou.ac.kr>
2022-09-13 17:01:35 +0900

    current position map

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
 import React, { useState,useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from "expo-location";
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
 import { setStatusBarNetworkActivityIndicatorVisible } from 'expo-status-bar';
 
 export default function App() {
-	const [city,setCity] = useState("Loading...");
-    const [district, setDistrict]=useState(); //구
+    const [city,setCity]=useState();
+    const [district,setDistrict]=useState();
     const [location, setLocation] = useState();
+    const [coords,setCoords]=useState();
  	const [ok, setOk] = useState(true);
-    const [initialRegion,setinitialRegion]=useState({
-    latitude: 35.91395373474155,
-    longitude: 127.73829440215488,
-    latitudeDelta: 0.001,
-    longitudeDelta: 0.001,
-    })
+    const [initialRegion,setinitialRegion]=useState();
  	const ask = async ()=>{
         const {granted} = await Location.requestForegroundPermissionsAsync();
         if(!granted){
             setOk(false);
         }
     const {coords: {latitude,longitude}} = await Location.getCurrentPositionAsync({accuracy :5}); //coords를 통해 현재 위치의 좌표 받기
+    const tmpcoords=await {latitude,longitude};
+    setCoords(tmpcoords);
     const location=await Location.reverseGeocodeAsync(
         {latitude,longitude},
         {useGoogleMaps: false}//구글맵지도 사용여부
     );
-    console.log(latitude,longitude);
+    setLocation(location);
+    if(location[0].city!=null){
     setCity(location[0].city);
+    }
+    else{
+        setCity(location[0].region)
+    }
     setDistrict(location[0].district);
+    setinitialRegion({
+        latitude:tmpcoords.latitude,
+        longitude:tmpcoords.longitude,
+        latitudeDelta:0.02,
+        longitudeDelta:0.02
+    })
     };
  	useEffect(() => {
     	ask();
@@ -35,15 +44,23 @@ export default function App() {
 return(
 <View style={styles.container}>
     <View style={styles.city}></View>
-    <Text style={styles.cityName}>{city}</Text>
+    <Text style={styles.city}>{city}</Text>
     <Text style={styles.districtName}>{district}</Text>
-    <MapView
-    initialRegion={initialRegion}
-    style={[styles.map]}
-    provider={PROVIDER_GOOGLE}
-    showsUserLocation={true}
-    showsMyLocationButton={true}
-    />
+<MapView
+region={initialRegion}
+style={[styles.map]}
+showsUserLocation={true}
+showsMyLocationButton={true}
+>
+    <Marker
+    coordinate={{
+        latitude:37.2783333,
+        longitude:127.046866
+    }}
+    title="this is a marker"
+    description='this is a marker example'
+/>
+    </MapView>
 </View>
 );
 }
