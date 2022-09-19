8c1da2e283a74460c74bf292fd72736b2be0be7a
hay0107 <hay0107@ajou.ac.kr>
2022-09-19 10:31:03 +0900

    0919 commit

--- App.js
+++ App.js
@@ -1,65 +1,99 @@
 import React, { useState,useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from "expo-location";
-import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps';
-import { setStatusBarNetworkActivityIndicatorVisible } from 'expo-status-bar';
+import {DOMParser} from 'xmldom';
+import MapView, {Marker} from 'react-native-maps';
 
 export default function App() {
-    const [city,setCity]=useState();
-    const [district,setDistrict]=useState();
-    const [location, setLocation] = useState();
-    const [coords,setCoords]=useState();
- 	const [ok, setOk] = useState(true);
     const [initialRegion,setinitialRegion]=useState();
+    const [result, setResult] = useState([]);
+    const [markers, setMarkers] =useState([]);
  	const ask = async ()=>{
         const {granted} = await Location.requestForegroundPermissionsAsync();
-        if(!granted){
-            setOk(false);
-        }
     const {coords: {latitude,longitude}} = await Location.getCurrentPositionAsync({accuracy :5}); //coords를 통해 현재 위치의 좌표 받기
-    const tmpcoords=await {latitude,longitude};
-    setCoords(tmpcoords);
-    const location=await Location.reverseGeocodeAsync(
-        {latitude,longitude},
-        {useGoogleMaps: false}//구글맵지도 사용여부
-    );
-    setLocation(location);
-    if(location[0].city!=null){
-    setCity(location[0].city);
-    }
-    else{
-        setCity(location[0].region)
-    }
-    setDistrict(location[0].district);
     setinitialRegion({
-        latitude:tmpcoords.latitude,
-        longitude:tmpcoords.longitude,
+        latitude:latitude,
+        longitude:longitude,
         latitudeDelta:0.02,
         longitudeDelta:0.02
     })
     };
+    const searchStation = async()=>{
+        console.log("search")
+        try{
+          var xhr = new XMLHttpRequest();
+          var url = 'http://apis.data.go.kr/6410000/busstationservice/getBusStationList'; /*URL*/
+          var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'UkgvlYP2LDE6M%2Blz55Fb0XVdmswp%2Fh8uAUZEzUbby3OYNo80KGGV1wtqyFG5IY0uwwF0LtSDR%2FIwPGVRJCnPyw%3D%3D'; /*Service Key*/
+          queryParams += '&' + encodeURIComponent('keyword') + '=' + encodeURIComponent('아주대병원'); /**/
+          xhr.open('GET', url + queryParams);
+          xhr.onreadystatechange = function () {
+              if (this.readyState == 4) {
+              let xmlParser = new DOMParser();
+              let xmlDoc = xmlParser.parseFromString(this.responseText, "text/xml");
+              let i = 0;
+              while(1){
+                var tmpnode = new Object();
+                
+                console.log(i)
+                tmpnode.id = xmlDoc.getElementsByTagName("stationId")[i].textContent;
+                tmpnode.name = xmlDoc.getElementsByTagName("stationName")[i].textContent;
+                tmpnode.x = xmlDoc.getElementsByTagName("x")[i].textContent;
+                tmpnode.y = xmlDoc.getElementsByTagName("y")[i].textContent;
+                
+               result.push(tmpnode);
+                setResult(result);
+              
+                i++;
+                if(xmlDoc.getElementsByTagName("stationId")[i]==undefined) break;
+              }
+            }
+          }
+        xhr.send();
+        }
+        catch(err){
+          alert(err);
+        }
+    };
+    const printMarker=async()=>{
+        const resultMarker=result.map((markerItem)=>{
+            return{
+                key:markerItem.id,
+                coordinate:{
+                    latitude:markerItem.y,
+                    longitude:markerItem.x
+                },
+            };
+        });
+        setMarkers(resultMarker);
+    }
  	useEffect(() => {
     	ask();
-  	}, []);
+  	 }, []);
+    searchStation()
+    .then(printMarker)
+    .catch("err");
 return(
 <View style={styles.container}>
-    <View style={styles.city}></View>
-    <Text style={styles.city}>{city}</Text>
-    <Text style={styles.districtName}>{district}</Text>
 <MapView
-region={initialRegion}
-style={[styles.map]}
-showsUserLocation={true}
-showsMyLocationButton={true}
->
+    region={initialRegion}
+    style={[styles.map]}
+    showsUserLocation={true}
+    showsMyLocationButton={true}
+    >
     <Marker
     coordinate={{
         latitude:37.2783333,
         longitude:127.046866
     }}
-    title="this is a marker"
-    description='this is a marker example'
 />
+{markers.map((item)=>{
+    <Marker
+    key={item.key}
+    coordinate={item.coordinate}
+/>
+}
+)}
+
     </MapView>
 </View>
 );
@@ -71,24 +105,6 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-    },
-    city:{
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontSize : 50
-    },
-    cityName:{
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontSize : 50
-    },
-    districtName:{
-        flex:1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        fontSize : 40
     },
     map:{
         flex:1,
